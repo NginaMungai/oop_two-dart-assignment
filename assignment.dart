68235eda3428b0fde7a5defdf40b400949fdb28d
Ngina Mungai <nginamungai8@gmail.com>
2024-02-29 21:33:32 +0300

    First Version

---
+++ assignment.dart
@@ -0,0 +1,25 @@
+void main() {
+  //int data type- used to store numeric value on dart
+  int age = 14;
+  print(age);
+
+  //Double data type- stores numeric value on dart
+  double mass = 36.4;
+  print(mass);
+
+  //String data type- stores text data in the program
+  String greeting = 'Hello World';
+  print(greeting);
+
+  //list data type- stores multiple values without creating multiple variables
+  List<int> marks = [50, 80, 40, 90];
+  print(marks);
+
+  //map data type- used to present key-value pairs
+  Map<String, int> scores = {'Alice': 60, 'Martin': 90, 'Susan': 50};
+  print(scores);
+
+  //boolean data type- holds either true or false value
+  bool married = false;
+  print(married);
+}
